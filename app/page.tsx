ae844ff0db27ab6d514d6ee9f6529b62bcb4115d
Crystal Tai <crystaltai.xyz@gmail.com>
2025-02-02 23:08:32 -0500

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
 import ReactPills from '@/components/ReactPills';
 import DndPills from '@/components/DndPills';
+
+import { GithubIcon } from 'lucide-react';
 
 export type Pill = {
   id: number;
@@ -22,19 +25,39 @@ export default function Home() {
   ];
 
   return (
-    <main className="flex flex-col items-center justify-items-center min-h-screen p-8 gap-12 font-[family-name:var(--font-geist-sans)]">
-      <div className="flex flex-col items-center justify-items-center gap-2">
-        <h1 className="text-2xl font-medium">HTML5 Drag and Drop</h1>
-        <div className="mt-4 text-sm text-gray-600">Uses the native HTML5 drag and drop API.</div>
-        <ReactPills pills={PILLS} />
-      </div>
-      <div className="flex flex-col items-center justify-items-center gap-2">
-        <h1 className="text-2xl font-medium">dnd-kit/core and dnd-kit/sortable</h1>
-        <div className="mt-4 text-sm text-gray-600">
-          Uses dnd-kit/core and dnd-kit/sortable using a line indicator for drop position.
+    <div className="flex flex-col items-center justify-items-center min-h-screen p-8 pt-24 gap-12 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex-1 flex flex-col items-center justify-items-center gap-12">
+        <div className="flex flex-col items-center justify-items-center gap-2">
+          <h1 className="text-2xl font-medium">HTML5 Drag and Drop</h1>
+          <div className="mt-4 text-sm text-gray-600">Uses the native HTML5 drag and drop API.</div>
+          <ReactPills pills={PILLS} />
         </div>
-        <DndPills pills={PILLS} />
-      </div>
-    </main>
+        <div className="flex flex-col items-center justify-items-center gap-2">
+          <h1 className="text-2xl font-medium">dnd-kit/core and dnd-kit/sortable</h1>
+          <div className="mt-4 text-sm text-gray-600">
+            Uses dnd-kit/core and dnd-kit/sortable using a line indicator for drop position.
+          </div>
+          <DndPills pills={PILLS} />
+        </div>
+      </main>
+      <footer className="flex flex-col items-center justify-items-center gap-2">
+        <div>
+          <Link
+            href="https://github.com/crystaltai/shadcn-drag-and-drop"
+            className="hover:underline flex items-center gap-1"
+            target="_blank"
+          >
+            <GithubIcon className="w-5 h-5" />
+            <span>Code</span>
+          </Link>
+        </div>
+        <p className="text-gray-600">
+          Built by{' '}
+          <Link href="https://x.com/crystaltaixyz" className="hover:underline" target="_blank">
+            @crystaltaixyz
+          </Link>
+        </p>
+      </footer>
+    </div>
   );
 }
